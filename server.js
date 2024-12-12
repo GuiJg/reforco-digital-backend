79304d68fccfecf5ec16daad37386383bf82e5a2
GuiJg <j.gu1lh3rm325@gmail.com>
2024-12-12 18:57:54 -0300

    backend update

--- server.js
+++ server.js
@@ -6,25 +6,28 @@ const errorMiddleware = require("./middleware/errorMiddleware");
 const authRoute = require("./routes/authRoute");
 
 const app = express();
-const MONGO_URL = process.env.MONGO_URL;
-const PORT = process.env.PORT;
-const API_URL = process.env.API_URL;
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(errorMiddleware);
 
-app.use('/api/auth', authRoute);
+app.use("/api/auth", authRoute);
 
-mongoose.connect(MONGO_URL).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Servidor está rodando na porta ${PORT}`);
-    console.log("API:", API_URL);
-  });
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@reforcoadm.io4nj.mongodb.net/reforco-backend?retryWrites=true&w=majority&appName=reforcoAdm"
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("O servidor esta rodando na porta 3000");
+    });
+    app.get("/", (req, res) => {
+      res.send("O servidor esta rodando na porta 3000");
+    });
+    console.log("Servidor conectado ao MongoDB");
+  })
 
-  app.get("/", (req, res) => {
-    res.send("Servidor conectado ao MongoDB");
+  .catch((error) => {
+    console.log(error);
   });
-}); 
